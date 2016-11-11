0fe3eed33b6f12c385ce7f7daf4df47998d4d3ff
Mohammad Helal <mas_sina@hotmail.com>
2016-11-11 17:08:24 +0300

    The initial solution from visual studio

---
+++ TotaMoviesRental/TotaMoviesRental/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TotaMoviesRental.MvcApplication" Language="C#" %>
---
+++ TotaMoviesRental/TotaMoviesRental/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TotaMoviesRental.Startup))]
+namespace TotaMoviesRental
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
