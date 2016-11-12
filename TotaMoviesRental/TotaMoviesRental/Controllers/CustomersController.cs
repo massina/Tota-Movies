21d47e0cec40421190fbe7bc75ccc74e8caf8235
Mohammad Helal <mas_sina@hotmail.com>
2016-11-12 13:57:31 +0300

    Edit Customer

--- TotaMoviesRental/TotaMoviesRental/Controllers/CustomersController.cs
+++ TotaMoviesRental/TotaMoviesRental/Controllers/CustomersController.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using System.Web.Mvc;
 using TotaMoviesRental.Models;
+using TotaMoviesRental.ViewModels;
 
 namespace TotaMoviesRental.Controllers
 {
@@ -20,6 +21,49 @@ namespace TotaMoviesRental.Controllers
             var customers = _context.Customers.Include(c => c.MembershipType).ToList();
 
             return View(customers);
+        }
+
+        public ActionResult New()
+        {
+            var viewModel = new CustomerFormViewModel
+            {
+                MembershipTypes = _context.MembershipTypes.ToList(),
+                Customer = new Customer()
+            };
+
+            return View("CustomerForm", viewModel);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+            if (customer == null)
+                return HttpNotFound();
+            var viewModel = new CustomerFormViewModel
+            {
+                Customer = customer,
+                MembershipTypes = _context.MembershipTypes.ToList()
+            };
+
+            return View("CustomerForm", viewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Save(Customer customer)
+        {
+            if (customer.Id == 0)
+            _context.Customers.Add(customer);
+            else
+            {
+                var customerInDb = _context.Customers.Single(m => m.Id == customer.Id);
+                customerInDb.Name = customer.Name;
+                customerInDb.Birthdate = customer.Birthdate;
+                customerInDb.MembershipTypeId = customer.MembershipTypeId;
+                customerInDb.IsSubscribedToNewsLetter = customer.IsSubscribedToNewsLetter;
+            }
+            _context.SaveChanges();
+            return RedirectToAction("Index");
         }
 
         public ActionResult Details(int id)
---
+++ TotaMoviesRental/TotaMoviesRental/ViewModels/CustomerFormViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using TotaMoviesRental.Models;
+
+namespace TotaMoviesRental.ViewModels
+{
+    public class CustomerFormViewModel
+    {
+        public IEnumerable<MembershipType> MembershipTypes { get; set; }
+        public Customer Customer { get; set; }
+    }
+}
